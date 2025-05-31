79af7049ca4580fe08ead60f9b7e135b1d2d207c
Raja Adnan Ahmed <152651292+WATCHERsee@users.noreply.github.com>
2025-06-01 00:28:02 +0500

    updated

---
+++ script.js
@@ -0,0 +1,10 @@
+
+
+
+
+const cursor = document.querySelector('.cursor');
+
+document.addEventListener('mousemove', e => {
+  cursor.style.top = e.clientY + 'px';
+  cursor.style.left = e.clientX + 'px';
+});
